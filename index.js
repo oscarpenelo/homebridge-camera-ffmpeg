532b30de400e27511380051733a77969fee56ae5
Oscar Penelo <oscar.penelo@gmail.com>
2020-05-03 10:23:39 +0200

    0.1.30 version

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ ffmpegPlatform.prototype.didFinishLaunching = function() {
       self.switch.getCharacteristic(Characteristic.LockTargetState)
           .on('set', self.setlocker.bind(self))
 
-      self.switch.getCharacteristic(Characteristic.LockCurrentState).updateValue(0)
+      self.switch.getCharacteristic(Characteristic.LockCurrentState).updateValue(1)
 
 
       var cameraSource = new FFMPEG(hap, cameraConfig, self.log, videoProcessor, interfaceName);
@@ -188,12 +188,12 @@ ffmpegPlatform.prototype.setlocker = function  (turnOn, callback) {
     rpio.write(this.lockerpin, rpio.LOW);
     rpio.sleep(this.lockerseconds);
     rpio.write(this.lockerpin, rpio.HIGH);
-    this.switch.getCharacteristic(Characteristic.LockCurrentState).updateValue(0)
+    this.switch.getCharacteristic(Characteristic.LockCurrentState).updateValue(1)
 
     callback()
   }
   else{
-    this.switch.getCharacteristic(Characteristic.LockCurrentState).updateValue(0)
+    this.switch.getCharacteristic(Characteristic.LockCurrentState).updateValue(1)
 
   }
 }
